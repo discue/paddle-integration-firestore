f516dc775d2b78069ef4b665276c3165ff2bdd19
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-08-23 20:23:15 +0200

    tests: add emulator runner

---
+++ test/module-runner.js
@@ -0,0 +1,64 @@
+const { spawn } = require('child_process')
+const { platform } = require('os')
+
+const isUnix = platform().includes('win') ? false : true
+
+module.exports = class Runner {
+    start(binary, args, cwd, waitFor, logOutput = true) {
+        return new Promise((resolve) => {
+            this._process = spawn(binary, args, { windowsHide: true, env: null, cwd })
+
+            this._waitForOutputAndCallBack(this._process.stdout, waitFor, resolve)
+
+            if (logOutput) {
+                this._process.stdout.on('data', (data) => {
+                    console.log('data', data.toString())
+                });
+            }
+            
+            this._process.stderr.on('data', (data) => {
+                console.log('err', data.toString())
+            });
+
+            process.on('SIGHUP', () => {
+                this.stop()
+            })
+        })
+    }
+
+    _waitForOutputAndCallBack(stream, message, callback) {
+        stream.on('data', function fn(data) {
+            if (data.toString('utf-8').indexOf(message) >= 0) {
+                stream.removeListener('data', fn)
+                callback()
+            }
+        })
+    }
+
+    stop() {
+        console.log('stopping')
+        if (isUnix) {
+            return this._stopUnix()
+        } else {
+            return this._stopWin()
+        }
+    }
+
+    _stopUnix() {
+        return new Promise((resolve) => {
+            if (!this._process.killed) {
+                const kill = spawn('kill', [this._process.pid]);
+                kill.on('exit', resolve)
+            }
+        })
+    }
+
+    _stopWin() {
+        return new Promise((resolve) => {
+            if (!this._process.killed) {
+                const kill = spawn("taskkill", ["/pid", this._process.pid, '/f', '/t']);
+                kill.on('exit', resolve)
+            }
+        })
+    }
+}
