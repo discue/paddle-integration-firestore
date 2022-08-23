31759b41dd377cd3f55ba45cb650735a3ce8281e
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-08-23 20:23:32 +0200

    tests: add tests fixtures

---
+++ test/fixtures/subscription-cancelled.js
@@ -0,0 +1,19 @@
+module.exports = {
+    alert_id: '439273720',
+    alert_name: 'subscription_cancelled',
+    cancellation_effective_date: '2021-09-04',
+    checkout_id: '3-21a8f3aba7b822b-5e53440387',
+    currency: 'EUR',
+    email: 'jocelyn.goodwin@example.org',
+    event_time: '2021-08-08 09:47:33',
+    linked_subscriptions: '3, 5, 9',
+    marketing_consent: '0',
+    passthrough: 'Example String',
+    quantity: '86',
+    status: 'deleted',
+    subscription_id: '2',
+    subscription_plan_id: '8',
+    unit_price: '3.25',
+    user_id: '9',
+    p_signature: 'r'
+}
---
+++ test/fixtures/subscription-created.js
@@ -0,0 +1,23 @@
+module.exports = {
+    alert_id: '2014203184',
+    alert_name: 'subscription_created',
+    cancel_url: 'https://sandbox-my.paddle.com/subscription/cancel?user=9&subscription=5&hash=13f3e703caa74b5dd758faf3a24b415fd32e245d',
+    checkout_id: '3-d557ce34ede543e-9ac21077b0',
+    currency: 'USD',
+    email: 'leffler.zola@example.org',
+    event_time: '2021-08-08 07:46:58',
+    linked_subscriptions: '3, 1, 2',
+    marketing_consent: '',
+    next_bill_date: '2021-08-29',
+    passthrough: 'Example String',
+    quantity: '17',
+    source: 'Trial',
+    status: 'active',
+    subscription_id: '2',
+    subscription_plan_id: '8',
+    unit_price: '3.25',
+    update_url: 'https://sandbox-my.paddle.com/subscription/update?user=1&subscription=6&hash=7d49e417e9492ad53a3fd17bc97f63927c2c0973',
+    user_id: '9',
+    p_signature: 'r'
+}
+
