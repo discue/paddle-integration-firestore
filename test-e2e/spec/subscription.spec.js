855a32a980997c4162f321f96dc9bd5f1958e05d
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-09-03 23:21:02 +0200

    tests: add test case for sub created and cancelled

---
+++ test-e2e/spec/subscription.spec.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const { test, expect } = require('@playwright/test')
+const emulatorRunner = require('../../test/emulators-runner')
+const hookRunner = require('../hook-server-runner')
+const hookTunnelRunner = require('../hook-tunnel-runner')
+const testPageRunner = require('../test-page-runner')
+
+const Subscriptions = require('../../lib/index')
+const subscriptions = new Subscriptions('subscriptions')
+
+const storageResource = require('../../lib/firestore/nested-firestore-resource')
+const storage = storageResource({ documentPath: 'subscriptions', resourceName: 'subscription' })
+
+test.beforeAll(emulatorRunner.start)
+test.afterAll(emulatorRunner.stop)
+
+test.beforeAll(hookRunner.start)
+test.afterAll(hookRunner.stop)
+
+test.beforeAll(hookTunnelRunner.start)
+test.afterAll(hookTunnelRunner.stop)
+
+test.beforeAll(testPageRunner.start)
+test.afterAll(testPageRunner.stop)
+
+test.beforeAll(() => {
+    return subscriptions.addSubscriptionPlaceholder(['4815162342'])
+})
+
+test('test receives and stores webhooks', async ({ page }) => {
+    await page.goto('http://localhost:3333/checkout.html')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="postcodeInput"]').click()
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="postcodeInput"]').fill('12345')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="postcodeInput"]').press('Enter')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="CARD_PaymentSelectionButton"]').click()
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="cardNumberInput"]').click()
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="cardNumberInput"]').fill('4242 4242 4242 4242')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="cardNumberInput"]').press('Tab')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="cardholderNameInput"]').fill('Muller')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="cardholderNameInput"]').press('Tab')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="expiryMonthInput"]').fill('120')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="expiryMonthInput"]').press('Tab')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="expiryYearInput"]').fill('2025')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="expiryYearInput"]').press('Tab')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="cardVerificationValueInput"]').fill('123')
+    await page.frameLocator('iframe[name="paddle_frame"]').locator('[data-testid="cardVerificationValueInput"]').press('Enter')
+
+    await page.waitForSelector('#paddleSuccess', { state: 'visible', timeout: 50000 })
+    await page.waitForTimeout(30000)
+
+    let subscription = await storage.get(['4815162342'])
+    expect(subscription).not.toBeFalsy()
+
+    console.log({ subscription })
+
+    let isActive = await subscriptions.isSubscriptionActive(subscription)
+    expect(isActive).toBeTruthy()
+
+    await page.goto(subscription.cancel_url)
+    await page.locator('text=Cancel Subscription').click()
+
+    await page.waitForTimeout(10000)
+
+    // subscription still active today
+    subscription = await storage.get(['4815162342'])
+    isActive = await subscriptions.isSubscriptionActive(subscription)
+    expect(isActive).toBeTruthy()
+
+    // but cancelled next month
+    isActive = await subscriptions.isSubscriptionActive(subscription, new Date(new Date().getTime() + 1000 * 3600 * 24 * 35))
+    expect(isActive).toBeFalsy()
+})
