ffcd312616a9bb6dfa1be24b19118d5a62ff2545
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-09-03 23:17:09 +0200

    tests: add playwright config

---
+++ playwright.config.js
@@ -0,0 +1,26 @@
+// playwright.config.js
+// @ts-check
+const { devices } = require('@playwright/test')
+const headless = true
+
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+    testDir: 'test-e2e/spec',
+    testMatch: '*.spec.js',
+    reportSlowTests: null,
+    timeout: 1000 * 60 * 2,
+    forbidOnly: !!process.env.CI,
+    use: {
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure',
+        video: 'on-first-retry'
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'], headless },
+        }
+    ],
+}
+
+module.exports = config
