e49452a22dbf997db50a81dd75504deed31e3658
stfsy <s.pfaffel@gmail.com>
2022-12-31 15:21:56 +0100

    chore(firebase): prevent port collisions

--- run-emulators.sh
+++ run-emulators.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export NODE_ENV=
-export FIRESTORE_EMULATOR_HOST=localhost:9999
+export FIRESTORE_EMULATOR_HOST=localhost:11111
 
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # if running on windows make sure emulators are stopped before running them again
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export FIREBASE_AUTH_EMULATOR_HOST=localhost:9099
-export FIRESTORE_EMULATOR_HOST=localhost:9999
+export FIRESTORE_EMULATOR_HOST=localhost:11111
 export FIREBASE_CONFIG='{ "projectId": "discue-io-dev", "storageBucket": "discue-io-dev.appspot.com" }'
 export NODE_ENV='ci'
 
